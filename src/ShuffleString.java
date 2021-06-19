c492ee9ed9e60aff4c332e103ba3774febb40c94
ainazimo <ainazik.momunalieva@iaau.edu.kg>
2021-06-19 12:47:24 +0600

    18th June 2021

--- src/ShuffleString.java
+++ src/ShuffleString.java
@@ -1,11 +1,15 @@
+import java.util.HashMap;
+
 public class ShuffleString {
     public String restoreString(String s, int[] indices) {
-        char[] str = s.toCharArray();
-        for (int i = 0 ; i < indices.length ; i++)
-            str[indices[i]] = s.charAt(i);
-        return String.valueOf(str);
+        StringBuilder sb = new StringBuilder();
+        HashMap<Integer, Character> map = new HashMap<Integer, Character>();
+        for(int i = 0; i < indices.length; i++){
+            map.put(indices[i], s.charAt(i));
+        }
+        map.entrySet().forEach(x -> sb.append(x.getValue()));
+        return sb.toString();
     }
-
     /*
     char[] str = s.toCharArray();
     for (int i = 0 ; i < indices.length ; i++)
