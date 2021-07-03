24db59ff9f1f269f7e5bcbdf13c1e46cbfc9828e
ainazimo <ainazik.momunalieva@iaau.edu.kg>
2021-07-03 15:12:31 +0600

    3rd July 2021

---
+++ src/ReverseStr.java
@@ -0,0 +1,13 @@
+public class ReverseStr {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right){
+            char tmp = s[right];
+            s[right] = s[left];
+            s[left] = tmp;
+            left++;
+            right--;
+        }
+    }
+}
