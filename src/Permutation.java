5114ff2bac84cb29a03658d9de950eac0f089c35
ainazimo <ainazik.momunalieva@iaau.edu.kg>
2021-07-10 20:04:52 +0600

    +

---
+++ src/Permutation.java
@@ -0,0 +1,12 @@
+public class Permutation {
+    public static void main(String[] args) {
+
+    }
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
