af7ff1fab357076c23514e7e6fece21ad62e2242
Ainazik <ainazik.momunalieva@iaau.edu.kg>
2021-11-19 22:17:21 +0600

    +

---
+++ src/ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,20 @@
+public class ConvertBinaryNumberInALinkedListToInteger {
+    public int getDecimalValue(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+
+        while (head != null){
+            sb.append(head.val);
+            head = head.next;
+        }
+        return Integer.parseInt(sb.toString(), 2);
+    }
+
+    public int getDecimalValueS(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = num * 2 + head.next.val;
+            head = head.next;
+        }
+        return num;
+    }
+}
